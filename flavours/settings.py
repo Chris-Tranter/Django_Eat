b8b544b74a9b5405eec63ab87249c4cb48564adb
Christopher Tranter <mr_crock@hotmail.co.uk>
2024-06-21 13:46:22 +0000

    create superuser link database

--- flavours/settings.py
+++ flavours/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +28,6 @@ SECRET_KEY = 'django-insecure-mc1=u+ii#1^9&(*lv+zvq0)(+*9hv7#mr-o$)jcgpi9ik4lx&4
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
 ALLOWED_HOSTS = ['8000-christranter-djangoeat-pjwzx6w0fbn.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
@@ -74,13 +77,16 @@ WSGI_APPLICATION = 'flavours.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+#}
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
